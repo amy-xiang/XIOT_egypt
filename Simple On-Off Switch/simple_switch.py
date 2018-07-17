83a0646f02062d3982cab7601892e9eecbf0d5dd
HelenG <gezahegn@ualberta.ca>
2018-07-17 09:03:19 -0400

    ERROR: Failed fetch
    ERROR: Cannot fetch a 404 webpage.
    
    OTHER CHANGES:
    - Linked a js file
    - Created a switchOn and switchOff function.

---
+++ Simple On-Off Switch/simple_switch.py
@@ -0,0 +1,7 @@
+# You have you make a web page with a two Switch to turn our xChip on and off using http requests to send json messages.
+import urllib.request
+
+if __name__ == "main":
+    pass
+
+main()
