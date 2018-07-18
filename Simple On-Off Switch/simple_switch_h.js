38674ff0e9abf804964a2b62c5aede32c70f04a1
HelenG <gezahegn@ualberta.ca>
2018-07-18 10:18:57 +0200

    Created makeRequest()
    -Noticed repetitive code so condensed the request into its own function called makeRequest where the url and data are passed as arguments.
    -Added comments to simple_switch js and html file.

--- Simple On-Off Switch/simple_switch_h.js
+++ Simple On-Off Switch/simple_switch_h.js
@@ -1,23 +1,26 @@
+// There are two buttons on the webpage--on and off. Each button has its respective function where 
+// the url and data is defined and then passed into the makeRequest function. 
+
 function switchOn() {
+    // To switch the xChip on, define the url and data as follows
     var url = 'http://blynk-cloud.com/2674d23861f547c990b570a742954151/update/D14';
     var data = {payload: ["1"]};
 
-    fetch(url, {
-        method: 'PUT', 
-        body: JSON.stringify(data), 
-        headers: {
-            'Content-Type': 'application/json' 
-        }
-    })
-    .then(res => res.json())
-    .catch(error => console.error('Error:', error))
-    .then(response => console.log('Success:', response));
+    // Fetch a PUT Request
+    makeRequest(url, data); 
 }
 
 function switchOff() {
+    // To switch the xChip off, define the url and data as follows
     var url = 'http://blynk-cloud.com/2674d23861f547c990b570a742954151/update/D4';
     var data = {payload: ["0"]};
 
+    // Fetch a PUT Request
+    makeRequest(url, data); 
+}
+
+function makeRequest(url, data) {
+    // PUT request
     fetch(url, {
         method: 'PUT', 
         body: JSON.stringify(data), 
@@ -28,4 +31,5 @@ function switchOff() {
     .then(res => res.json())
     .catch(error => console.error('Error:', error))
     .then(response => console.log('Success:', response));
+
 }
