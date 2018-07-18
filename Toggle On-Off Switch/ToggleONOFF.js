8125566dca0d1e2e09e1eeccea4b1baf07e0b416
Amy Xiang <33043340+amy-xiang@users.noreply.github.com>
2018-07-18 05:11:44 -0600

    Amy's Toggle Finished

---
+++ Toggle On-Off Switch/ToggleONOFF.js
@@ -0,0 +1,34 @@
+function toggle_switch(toggle)
+{
+    if (toggle == 0)
+    {
+        toggle = 1;
+        var url = 'http://blynk-cloud.com/2674d23861f547c990b570a742954151/update/D14';
+        var data = {payload: ["1"]};
+
+        var output = document.getElementsByTagName("button")[0];
+        output.innerHTML = "OFF";
+        output.style.backgroundColor = "rgb(219, 87, 87)";
+    
+        var textchange = document.getElementsByClassName('update')[0];
+        textchange.innerHTML = "xChip is Currently ON";
+    }
+    else
+    {
+        toggle = 0;
+        var url = 'http://blynk-cloud.com/2674d23861f547c990b570a742954151/update/D4'
+        var data = {payload: ["0"]};
+
+        var output = document.getElementsByTagName("button")[0];
+        output.innerHTML = "ON";
+        output.style.backgroundColor = "#4CAF50";
+
+        var textchange = document.getElementsByClassName('update')[0];
+        textchange.innerHTML = "xChip is Currently OFF";
+
+    }
+    return toggle;
+
+
+
+}
