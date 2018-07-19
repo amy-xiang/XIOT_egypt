ce46e3cb8e1fb66ca683c49bbeac8f926f815751
Amy Xiang <33043340+amy-xiang@users.noreply.github.com>
2018-07-19 06:40:59 -0600

    xhr

--- Simple On-Off Switch/Amy/SimpleONOFF.js
+++ Simple On-Off Switch/Amy/SimpleONOFF.js
@@ -1,26 +1,30 @@
 function switchon()
 {
     // variable declaration of needed assets
-    var url = 'http://blynk-cloud.com/2674d23861f547c990b570a742954151/update/D14';
-    var data = {payload: ["1"]};
+    var url = 'https://blynk-cloud.com/2674d23861f547c990b570a742954151/update/D14';
+    var data = ["1"];
+    var xhr = new XMLHttpRequest();
+    xhr.open('PUT', url, true);
+    xhr.send()
 
-    
-    fetch(url, {
-        method: 'PUT', 
-        body: JSON.stringify(data), 
-        headers: {
-            'Content-Type': 'application/json' 
-        }
-    })
-    .then(res => res.json())
-    .catch(error => console.error('Error:', error))
-    .then(response => console.log('Success:', response));
+
+
+    // fetch(url, {
+    //     method: 'PUT', 
+    //     body: JSON.stringify(data), 
+    //     headers: {
+    //         'Content-Type': 'application/json' 
+    //     }
+    // })
+    // .then(res => res.json())
+    // .catch(error => console.error('Error:', error))
+    // .then(response => console.log('Success:', response));
 
 }
 
 function switchoff()
 {
-    var url = 'http://blynk-cloud.com/2674d23861f547c990b570a742954151/update/D4';
+    var url = 'https://blynk-cloud.com/2674d23861f547c990b570a742954151/update/D4';
     var data = {payload: ["0"]};
 
     fetch(url, {
