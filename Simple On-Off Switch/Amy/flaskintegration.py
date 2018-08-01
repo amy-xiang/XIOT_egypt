c7abdade067c393020b09ff2b2f544c145b6cf39
Amy Xiang <33043340+amy-xiang@users.noreply.github.com>
2018-08-01 03:43:48 -0600

    amy's changes

--- Simple On-Off Switch/Amy/flaskintegration.py
+++ Simple On-Off Switch/Amy/flaskintegration.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+from simpleonoffscript import lightswitch
+
+on = '[1]'
+off = '[0]'
 
 app = Flask(__name__)
 
@@ -6,10 +10,17 @@ app = Flask(__name__)
 def index():
     return render_template("SimpleONOFF.html")
 
-@app.route('/light')
-def index():
-    
+@app.route('/lighton')
+def lighton():
+    lightswitch(on)
+    return render_template("SimpleONOFF.html")
 
+@app.route('/lightoff')
+def lightoff():
+    lightswitch(off)
+    return render_template("SimpleONOFF.html")
+
+    
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
     
--- Simple On-Off Switch/Amy/simpleonoffscript.py
+++ Simple On-Off Switch/Amy/simpleonoffscript.py
@@ -1,12 +1,9 @@
 import requests
 
-header = {'Content-Type':'application/json'}
+header = {
+    'Content-Type':'application/json',
+    }
 
-def lightswitch(payload):
-    response = requests.put('http://blynk-cloud.com/5a13704de48e4400b0a62214e8e7b354/update/D14', headers = header, data = payload)
-
-def outlet(payload):
-    response = requests.put('http://blynk-cloud.com/5a13704de48e4400b0a62214e8e7b354/update/D4', headers = header, data = payload)
-
-
+def lightswitch(data):
+    response = requests.put('http://blynk-cloud.com/5a13704de48e4400b0a62214e8e7b354/update/D14', headers = header, data = data)
 
